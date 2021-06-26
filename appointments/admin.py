f188fb456b06bc35b43f38f36de559a969f8bf0c
Yurii Birban <mrstream1@gmail.com>
2021-06-26 13:15:37 +0300

    Added new model - WorkSchedulePart, added ManyToMany relation between Master and WorkSchedulePart

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Master, Service, Appointment
+from .models import Master, Service, Appointment, WorkSchedulePart
 
 # Register your models here.
 admin.site.register(Master)
 admin.site.register(Appointment)
 admin.site.register(Service)
+admin.site.register(WorkSchedulePart)
--- appointments/models.py
+++ appointments/models.py
@@ -2,8 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class WorkSchedulePart(models.Model):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+    WEEKDAY_CHOICES = [
+        (MONDAY, 'Monday'),
+        (TUESDAY, 'Tuesday'),
+        (WEDNESDAY, 'Wednesday'),
+        (THURSDAY, 'Thursday'),
+        (FRIDAY, 'Friday'),
+        (SATURDAY, 'Saturday'),
+        (SUNDAY, 'Sunday'),
+    ]
+    weekday = models.IntegerField(
+        choices=WEEKDAY_CHOICES,
+    )
+    appointment_time = models.TimeField()
+
+    def __str__(self):
+        for weekday_choice in self.WEEKDAY_CHOICES:
+            if weekday_choice[0] == self.weekday:
+                return weekday_choice[1] + ' ' + self.appointment_time.strftime('%H:%M')
+
+
 class Master(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    work_schedule_parts = models.ManyToManyField(WorkSchedulePart, null=True)
 
     def __str__(self):
         return self.user.username
